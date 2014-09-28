34e892556cd5a65fe2fe7dba2ecb3bf349296f91
Charles Max Wood <chuck@devchat.tv>
2014-09-27 22:34:37 -0600

    fix up the sucesses to expire at the end of the day

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,45 @@
+var getDate = function() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+    dd='0'+dd
+  }
+
+  if(mm<10) {
+    mm='0'+mm
+  }
+
+  return mm+'/'+dd+'/'+yyyy;
+}
+
 angular.module("TodaySuccess", ['ngCookies'])
   .controller("AppCtrl", ["$cookieStore", "$scope", function($cookieStore, $scope) {
+    $scope.today = getDate();
     $scope.template = "templates/index.html";
-    $scope.doneToday = $cookieStore.get("doneToday")
+    $scope.doneToday = $cookieStore.get("doneToday");
+    if($scope.today != $cookieStore.get("today")) {
+      $scope.doneToday = [];
+      $cookieStore.remove("doneToday");
+      delete $scope.todaysGoal;
+      $cookieStore.remove("todaysGoal");
+      $cookieStore.put("today", getDate());
+    }
     if(typeof($scope.doneToday) == 'undefined' || $scope.doneToday == null) {
       $scope.doneToday = [];
     }
-    $scope.today = $cookieStore.get("today")
+    $scope.todaysGoal = $cookieStore.get("todaysGoal");
     $scope.submitForm = function() {
-      $scope.today = this.today;
-      $cookieStore.put('today', this.today)
+      $scope.todaysGoal = this.todaysGoal;
+      $cookieStore.put('todaysGoal', this.todaysGoal)
     }
     $scope.markSuccess = function() {
-      $scope.doneToday.push(this.today);
-      $cookieStore.put("doneToday", $scope.doneToday)
-      this.today = null;
+      $scope.doneToday.push(this.todaysGoal);
+      $cookieStore.put("doneToday", $scope.doneToday);
+      delete $scope.todaysGoal;
+      $cookieStore.remove("todaysGoal");
     }
   }
 ]).filter("orEllipsis", function() {
