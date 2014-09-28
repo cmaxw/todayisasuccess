21abaf01df9a6952fb760a951abc51750fae9e9c
Charles Max Wood <chuck@devchat.tv>
2014-09-27 20:45:02 -0600

    require 'haml'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'todayisasuccess'
+set :repo_url, 'git@github.com:cmaxw/todayisasuccess.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/todayisasuccess'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -26,7 +26,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,21 +1,6 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
-
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+role :app, %w{deploy@todayisasuccess.com}
+role :web, %w{deploy@todayisasuccess.com}
+role :db,  %w{deploy@todayisasuccess.com}
 
 
 # Custom SSH Options
@@ -33,7 +18,7 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #
 # And/or per server (overrides global)
 # ------------------------------------
-# server 'example.com',
+# server 'todayisasuccess.com',
 #   user: 'user_name',
 #   roles: %w{web app},
 #   ssh_options: {
